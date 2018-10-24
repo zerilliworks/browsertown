a9b352b5e4916ae1ebadf28ded1a0d646de26ee7
Armand Zerilli <armand@zeril.li>
2018-10-24 10:55:07 -0400

    Add browser tracker tag

--- browser/pages/_document.tsx
+++ browser/pages/_document.tsx
@@ -17,6 +17,21 @@ export default class MyDocument extends Document {
         <body className="m0 p0 w-full h-full overflow-hidden">
           <Main/>
           <NextScript/>
+          <script type="text/javascript" dangerouslySetInnerHTML={{__html: `
+          var _gauges = _gauges || [];
+            (function() {
+            var t   = document.createElement('script');
+            t.type  = 'text/javascript';
+            t.async = true;
+            t.id    = 'gauges-tracker';
+            t.setAttribute('data-site-id', '5bd0847cfd6d0d15889bd580');
+            t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
+            t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(t, s);
+          })();
+          `}}>
+          </script>
         </body>
       </html>
     )
