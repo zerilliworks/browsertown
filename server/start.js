0e8ca1cd352c8d4329a832df9f3ecc3fa1051213
Armand Zerilli <armand@zeril.li>
2019-01-20 19:17:44 -0500

    Forgot to update include path for server start script

--- server/start.js
+++ server/start.js
@@ -1,3 +1,3 @@
-const {startServer} = require('./dist/server2')
+const {startServer} = require('./dist/server')
 
 startServer(process.env.OPENSHIFT_NODEJS_PORT || process.env.VCAP_APP_PORT || process.env.PORT || process.argv[2] || 8765)
